68ee1f1e2fbdd08472214c49fab426fefeb88f9e
Sgiath <sgiath@sgiath.dev>
2024-03-12 13:40:33 +0000

    feat: archive version 11
    Signed-off-by: Sgiath <sgiath@sgiath.dev>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Asterion archive";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.inotify-tools
+          pkgs.beam.packages.erlang_26.elixir_1_16
+          pkgs.zip
+        ];
+
+        shellHook = ''
+          export ERL_AFLAGS="+pc unicode -kernel shell_history enabled"
+          export ELIXIR_ERL_OPTIONS="+sssdio 128"
+        '';
+      };
+    }
+  );
+}
+
